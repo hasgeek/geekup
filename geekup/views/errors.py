10568e4f4780f0249df626c6b923aca12ef1ca7f
Nigel Babu <nigelbabu@gmail.com>
2011-12-24 15:33:38 +0530

    New 403 error page

--- geekup/views/errors.py
+++ geekup/views/errors.py
@@ -8,3 +8,8 @@ from flask import render_template
 @app.errorhandler(404)
 def page_not_found(e):
         return render_template('404.html'), 404
+
+
+@app.errorhandler(403)
+def not_authorized(e):
+        return render_template('403.html'), 403
